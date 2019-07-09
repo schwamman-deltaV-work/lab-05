bb9ac1bb1d28c562ca927837c041e50fc3c27d00
Jonathon Schwamman <schwamman@msn.com>
2019-07-09 14:54:16 -0500

    Completed problem 3

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function sum(a, b) { //eslint-disable-line
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+console.log(sum(sum(1, 2)[0],3)[0]);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +53,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sum1 = sum(sum(a, b)[0],c)[0];
+  var product = multiply(multiply(a, b)[0], c)[0];
+  var messageOne = a + ' and ' + b + ' and ' + c + ' sum to ' + sum1 + '.';
+  var messageTwo = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.';
+  return [sum1, product, messageOne, messageTwo];
 }
 
+// console.log(sumAndMultiply (4,7,5));
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
